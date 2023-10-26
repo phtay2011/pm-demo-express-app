a88666ab73798a13551bf7fe2f3c82d5cc437934
paul <paul@open.gov.sg>
2023-10-26 17:04:23 +0800

    update

--- test/smoke.js
+++ test/smoke.js
@@ -7,8 +7,22 @@ export const options = {
 };
 
 export default () => {
-  const urlRes = http.get(
-    "http://ec2-13-229-135-220.ap-southeast-1.compute.amazonaws.com:3000/"
-  );
+  const url =
+    "http://ec2-13-229-135-220.ap-southeast-1.compute.amazonaws.com:3000/";
+  // Test 1 - ensure website is up
+  const websiteTest = http.get(url);
+
+  // Test 2 - ensure api can store data
+  const payload = JSON.stringify({
+    name: "paul",
+  });
+
+  const params = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const apiTest = http.post(url, payload, params);
   sleep(1);
 };
